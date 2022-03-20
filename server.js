7137ff4e35523695fdabfa6625d6225a0090b6e6
luis <luis.cervantes7399@alumnos.udg.mx>
2022-03-19 21:01:47 -0600

    he

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular'));
+app.use(express.static('./dist/angular-tour-of-heroes'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angular-tour-of-heroes/'}),
